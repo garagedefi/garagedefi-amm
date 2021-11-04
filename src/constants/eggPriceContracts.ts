ce71ba95412e1f5dcaf4b5186127660ac006d0ae
Mechanic <garagedefi@gmail.com>
2021-11-04 21:19:12 +0800

    Updated price based on new router

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xb06c05c36efea317cd6fffebf04671560def1867',
   busdAddress: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
-  lpAddress: '0xb99fe02313eb069cdef62aca5688d29af032b05b'
+  lpAddress: '0x8866BE760777B40f988Eaad72EC9e3447711F3Ba'
 }
 
 export default priceContracts
